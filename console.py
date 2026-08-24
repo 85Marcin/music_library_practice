83838d0904fcbdd872f7f7124015f798cf4b7587
Marcin <marcinegzmont@gmail.com>
2026-08-24 16:40:30 +0100

    Added artist_repository

--- console.py
+++ console.py
@@ -1,1 +1,13 @@
-# Use this file to test your repository functions 
+from models.artist import Artist
+from models.album import Album
+
+import repositories.artist_repository as artist_repository
+
+artist1 = Artist("John Coltrane")
+artist2 = Artist("Miles Davies")
+
+artist_repository.save(artist1)
+artist_repository.save(artist2)
+
+artists = artist_repository.select_all()
+
--- repositories/artist_repository.py
+++ repositories/artist_repository.py
@@ -1,4 +1,5 @@
 from db.run_sql import run_sql
+from models.artist import Artist
 
 def save(artist):
     sql = "INSERT INTO artists (name) VALUES (%s) RETURNING *"
@@ -8,3 +9,36 @@ def save(artist):
     artist.id = id
     return artist
 
+def select_all():
+    artists = []
+    sql = "SELECT * FROM artists"
+    results = run_sql(sql)
+    for row in results: 
+        artist = Artist(row['name'], row['id'])
+        artists.append(artist)
+    return artists
+
+def select(id):
+    artist = None
+    sql = "SELECT * FROM artists WHERE id = %s"
+    values = [id]
+    results = run_sql(sql, values)
+    
+    if results:
+        artist = Artist(results[0]['name'], results[0]['id'])
+
+    return artist
+
+def delete_all():
+    sql = "DELETE FROM artists"
+    run_sql(sql)
+    
+def delete(id):
+    sql = "DELETE FROM artists WHERE id =%s"
+    values = [id]
+    run_sql(sql, values)
+    
+def update(artist):
+    sql = "UPDATE artists SET name = %s WHERE id = %s"
+    values = [artist.name, artist.id]
+    run_sql(sql, values)
