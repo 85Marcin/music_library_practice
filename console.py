e0fe4e2fd0af63ba14dab15ce5bb35fe4e28dee8
Marcin <marcinegzmont@gmail.com>
2026-08-24 21:09:58 +0100

    Finished project

--- console.py
+++ console.py
@@ -2,12 +2,22 @@ from models.artist import Artist
 from models.album import Album
 
 import repositories.artist_repository as artist_repository
+import repositories.album_repository as album_repository
 
-artist1 = Artist("John Coltrane")
-artist2 = Artist("Miles Davies")
+album_repository.delete_all()
+artist_repository.delete_all()
 
-artist_repository.save(artist1)
-artist_repository.save(artist2)
+artist_1 = Artist('Oasis')
+artist_repository.save(artist_1)
 
-artists = artist_repository.select_all()
+album_1 = Album("Roll With It","Rock", artist_1)
+album_repository.save(album_1)
+album_2 = Album("Another Album", "Pop", artist_1)
+album_repository.save(album_2)
+
+album_1.genre = "Pop"
+album_repository.update(album_1)
+
+for album in album_repository.select_all():
+    print(album.__dict__)
 
